09768c151e2da6ea5e5f1ddaa6c488999b122660
LionTao <taojiachun980831@163.com>
2022-03-20 13:42:56 +0800

    week4

---
+++ week4/130-surrounded-regions.py
@@ -0,0 +1,53 @@
+"""
+给你一个 m x n 的矩阵 board ，由若干字符 'X' 和 'O' ，找到所有被 'X' 围绕的区域，并将这些区域里所有的 'O' 用 'X' 填充。
+"""
+from typing import List
+class Solution:
+    """
+    分析题意可知, 只要“O”触及边界就可以躲过被同化成“X”
+    因此我们先从边界开始扫描, 把幸存者标记为一个TEMP
+    然后把剩下的倒霉的“O”同化成“X”
+    最后还原TEMP
+    """
+    def solve(self, board: List[List[str]]) -> None:
+        """
+        Do not return anything, modify board in-place instead.
+        """
+        TEMP="t"
+        if not board or not board[0]:
+            return
+        row = len(board)
+        col = len(board[0])
+
+        def dfs(i, j):
+            board[i][j] = TEMP
+            for x, y in [(-1, 0), (1, 0), (0, -1), (0, 1)]:
+                tmp_i = i + x
+                tmp_j = j + y
+                if 1 <= tmp_i < row and 1 <= tmp_j < col and board[tmp_i][tmp_j] == "O":
+                    dfs(tmp_i, tmp_j)
+
+        for j in range(col):
+            # 第一行
+            if board[0][j] == "O":
+                dfs(0, j)
+            # 最后一行
+            if board[row - 1][j] == "O":
+                dfs(row - 1, j)
+
+        for i in range(row):
+            # 第一列
+            if board[i][0] == "O":
+                dfs(i, 0)
+            # 最后一列
+            if board[i][col-1] == "O":
+                dfs(i, col - 1)
+
+        for i in range(row):
+            for j in range(col):
+                # O 变成 X
+                if board[i][j] == "O":
+                    board[i][j] = "X"
+                # B 变成 O
+                if board[i][j] == TEMP:
+                    board[i][j] = "O"
