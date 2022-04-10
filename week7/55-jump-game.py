6a6d608555508a5171bdd510d0e96721bec31528
LionTao <taojiachun980831@163.com>
2022-04-10 21:06:19 +0800

    add week7

---
+++ week7/55-jump-game.py
@@ -0,0 +1,20 @@
+"""
+给定一个非负整数数组 nums ，你最初位于数组的 第一个下标 。
+
+数组中的每个元素代表你在该位置可以跳跃的最大长度。
+
+判断你是否能够到达最后一个下标。
+"""
+
+from typing import List
+
+
+class Solution:
+    def canJump(self, nums: List[int]) -> bool:
+        n, rightmost = len(nums), 0
+        for i in range(n):
+            if i <= rightmost:
+                rightmost = max(rightmost, i + nums[i])
+                if rightmost >= n - 1:
+                    return True
+        return False
